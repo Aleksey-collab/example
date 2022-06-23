3d3776f36f82ada2834f9cb900e4d4a70adad2ad
Aleksey <rgd666@yandex.ru>
2022-06-23 22:30:38 +0300

    add example

--- example005_conditionifelse/Program.cs
+++ example005_conditionifelse/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("введите имя пльзователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура, это же МАША!");
+}
+else
+{
+    Console.Write("привет, ");
+    Console.WriteLine(username);
+}
